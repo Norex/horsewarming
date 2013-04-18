e2e5ba4292587d0f15f3b1178b60611662bbc786
RyanNielson <ryan.nielson@gmail.com>
2013-04-18 20:23:44 -0300

    Remove debug statements

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,4 @@
 exports.index = function(req, res){
-  console.log('yaaa');
   var socketUrl = process.env.NODE_ENV ? process.env.SUBDOMAIN + '.nodejitsu.com' : 'http://localhost:' + (process.env.PORT || 3000);
-  console.log(socketUrl);
   res.render('index', { socketUrl: socketUrl });
 };
