79e0fd85d84c9e330d3758b4aecd1d75f8c094af
Ryan Nielson <ryan.nielson@gmail.com>
2013-04-22 15:43:48 -0300

    Cleaned up and got twitter working

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express'),
     http = require('http'),
     path = require('path'),
     twitter = require('./twitter'),
-    instagram = require('./instagram'),
     socketIo = require('socket.io');
 
 var app = express();
@@ -25,26 +24,10 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-// app.get('/subscribe', function(request, response){
-//   instagram.subscribe(io, request, response);
-// });
-app.get('/subscribe', function(req, res) {
-  instagram.subscribe(io, req, res);
-});
-
-app.post('/subscribe', function(req, res) {
-  io.sockets.emit('instagram_images', 'nahhhh3');
-  // io.sockets.emit('instagram_images', req);
-  res.writeHead(200);
-  res.end();
-});
 
 var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-
 var io = socketIo.listen(server);
-
-//twitter.run('cat', io);
-instagram.run('cat', io);
+twitter.run('#cat', io);
