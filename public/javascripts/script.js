576a3d3acd35d5501d8d8bd32996d03133067b3f
Ryan Nielson <ryan.nielson@gmail.com>
2013-04-23 16:39:16 -0300

    added vine embedded support.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -11,6 +11,7 @@
         keyword = window.currentKeyword,
         tweets = [];
     socket.on('twitter', function(data) {
+      console.log(data);
       processTweet(data);
     });
 
@@ -18,7 +19,9 @@
       picTwitter = scrapePicTwitter(data);
       twitpic = scrapeTwitPic(data);
       instagram = scrapeInstagram(data);
-      var new_picture = $('<div class="row"><div class="span12"><blockquote class="tweet">' + picTwitter + twitpic + instagram +'<img class="profile" src="' + data.user.profile_image_url + '"/><p>' + data.text.replace(new RegExp('(^|[^\\w\\d#])(' + keyword + ')(\\b|$)','ig'), '$1<strong>$2</strong>$3') + '</p><small>' + data.user.screen_name + '</small></p></blockquote></div></div>');
+      vine = scrapeVine(data);
+
+      var new_picture = $('<div class="row"><div class="span12"><blockquote class="tweet">' + picTwitter + twitpic + instagram + vine + '<img class="profile" src="' + data.user.profile_image_url + '"/><p>' + data.text.replace(new RegExp('(^|[^\\w\\d#])(' + keyword + ')(\\b|$)','ig'), '$1<strong>$2</strong>$3') + '</p><small>' + data.user.screen_name + '</small></p></blockquote></div></div>');
 
       new_picture.find('img.picture').error(function () {
         $(this).hide();
@@ -26,6 +29,20 @@
 
       $('#tweets').prepend(new_picture);
       $('#tweets > div').slice(50).remove();
+    };
+
+    var scrapeVine = function(data) {
+      var output = '';
+      try{
+        for(var i=0; i<data.entities.urls.length; i++){
+          if(/vine\.co\//.test(data.entities.urls[i].display_url)){
+            output += '<div class="picture-container"><iframe class="vine-embed" src="' + data.entities.urls[i].expanded_url + '/embed/simple" width="480" height="480" frameborder="0"></iframe><script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script></div>';
+          }
+        }
+      }
+      catch(e){ }
+
+      return output;
     };
 
     var scrapePicTwitter = function(data) {
@@ -46,7 +63,7 @@
         for(var i=0; i<data.entities.urls.length; i++){
           if(/twitpic\.com\//.test(data.entities.urls[i].display_url)){
             var pic = data.entities.urls[i].display_url.replace(/^twitpic\.com\/([a-zA-Z0-9]+)\/*.*/, "$1");
-            output += formatPhoto('http://twitpic.com/show/medium/' + pic, 'pic-twitpic');
+            output += formatPhoto('http://twitpic.com/show/large/' + pic, 'pic-twitpic');
           }
         }
       }
